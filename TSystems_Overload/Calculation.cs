92e02b71633a7724ec6ff881edd6e6b53ce2cffc
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-24 11:47:15 +0530

    Add project files.

---
+++ TSystems_Overload/Calculation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_Overload
+{
+    class Calculation
+    {
+        public int addition(int a, int b)
+        {
+            return a + b;
+
+        }
+        public int addition(int a, int b, int c)//methode overloading = same methode name different parameter
+        {
+            return a + b + c;
+
+        }
+        public double addition(int a, double b)
+        {
+            return a + b;
+        }
+
+        
+    }
+}
---
+++ TSystems_Overload/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystems_Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c = new Calculation();
+           
+            Console.WriteLine(c.addition(10,22));
+
+            Console.ReadLine();
+        }
+    }
+}
